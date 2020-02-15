617713c69150d7e69bd2522bfaccdeafbcfe4a6f
ndqchi <59836089+ndqchi@users.noreply.github.com>
2020-02-15 16:16:12 +0800

    Passport

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ var logger = require('morgan');
 var session = require('express-session');
 var FileStore = require('session-file-store')(session);
 
+var passport = require('passport');
+var authenticate = require('./authenticate');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
@@ -35,6 +38,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser('12345-67890-09875-54321'));
 
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(session({
 	name: 'session-id',
 	secret: '12345-67890-09876-54321',
@@ -47,22 +53,15 @@ app.use('/',indexRouter);
 app.use('/users',usersRouter);
 
 function auth (req, res, next) {
-    console.log(req.session);
+    console.log(req.user);
 
-    if (!req.session.user) {
+    if (!req.user) {
         var err = new Error('You are not authenticated!');
         err.status = 403;
-        return next(err);
+        next(err);
     }
     else {
-        if (req.session.user === 'authenticated') {
-            next();
-        }
-        else {
-            var err = new Error('You are not authenticated!');
-            err.status = 401;
-            return next(err);
-        }
+        next();
     }
 }
 
