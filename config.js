c6e0752782aa4a19b3af9df66f6c1cb59c754984
ndqchi <59836089+ndqchi@users.noreply.github.com>
2020-02-29 22:27:31 +0700

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
 	'secretKey' : '12345-67890-09876-54321',
-	'mongoUrl': 'mongodb://localhost:27017/conFusion'
+	'mongoUrl': 'mongodb://localhost:27017/conFusion',
+	'facebook': {
+		clientId: '507047303334871',
+		clientSecret: 'd2a17f5e0b19a2aebf852e0fb0cb420c'
+	}
 }
