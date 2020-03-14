08b5c922ddf04f8737fd3aad6402db83edfb1267
ndqchi <59836089+ndqchi@users.noreply.github.com>
2020-03-14 22:51:20 +0800

    Integrating React Client with Server

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ var leaderRouter = require('./routes/leaderRouter');
 var promotionRouter = require('./routes/promotionRouter');
 var uploadRouter = require('./routes/uploadRouter');
 var favoriteRouter = require('./routes/favoriteRouter');
+var commentRouter = require('./routes/commentRouter');
 
 const mongoose = require('mongoose');
 
@@ -63,6 +64,7 @@ app.use('/promotions',promotionRouter);
 app.use('/leaders',leaderRouter);
 app.use('/imageUpload',uploadRouter);
 app.use('/favorites', favoriteRouter);
+app.use('/comments', commentRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
